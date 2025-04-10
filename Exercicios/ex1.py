4948ddd07abaeca178e7ff75f48ae393dfc0426f
Canganjo <canganjoclemente2002@gmail.com>
2025-04-10 18:15:04 +0200

    p2

--- Exercicios/ex1.py
+++ Exercicios/ex1.py
@@ -1,1 +1,12 @@
-print('hello world');
+import speech_recognition as sr
+
+reconhecedor = sr.Recognizer()
+with sr.Microphone() as fonte:
+    print("Fala aí:")
+    audio = reconhecedor.listen(fonte)
+
+    try:
+        texto = reconhecedor.recognize_google(audio, language="pt-BR")
+        print("Você disse:", texto)
+    except sr.UnknownValueError:
+        print("Não entendi...")
